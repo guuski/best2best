f2fde878ecfe6316908ba62b13164ead238406c6
andrea.sangiorgio <andrea.sangiorgio@gmail.com>
2012-03-21 09:55:59 +0000

    refactoring plugin completo

---
+++ plugins/bp-review/loader.php
@@ -0,0 +1,56 @@
+<?php
+/*
+Plugin Name: bp-review
+Plugin URI:
+Description: 
+Version: 0.0.1
+Revision Date: MMMM DD, YYYY
+Requires at least: 
+Tested up to: 
+License: GNU General Public License 2.0 (GPL) http://www.gnu.org/licenses/gpl.html
+Author: 
+Author URI:
+Network: true
+*/
+
+/*
+-----------------------------------------
+Contenuto FILE:
+-----------------------------------------
+
+	- dichiara le COSTANTI per il plugin
+	- include (con la funz 'require') il file 'bp-review-loader.php'
+
+-----------------------------------------
+FILE, CLASSI, OGGETTI collegati 
+-----------------------------------------
+		
+	- [PHP file] 
+		'bp-review-loader.php' in 'includes/' 
+			
+-----------------------------------------
+FUNZIONI e HOOKS (BuddyPress - Bp)
+-----------------------------------------		
+	'bp_include'	--->	bp_review_init()
+	
+
+*/
+	
+//contiene il path alla cartella del plugin 
+define( 'BP_REVIEW_PLUGIN_DIR', dirname( __FILE__ ) );
+
+/**
+ * Carica il plguin solo se BuddyPress è presente.
+ * 
+ */
+function bp_review_init() 
+{
+	// poichè il plugin usa BP_Component, richiede la versione di BP 1.5 o successiva.
+	if ( version_compare( BP_VERSION, '1.5', '>' ) )
+		require( dirname( __FILE__ ) . '/includes/bp-review-loader.php' );
+}
+
+//
+add_action( 'bp_include', 'bp_review_init' );
+
+?>
