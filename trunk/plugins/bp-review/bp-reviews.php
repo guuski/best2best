63ab0b7972e42bf36fedb10ff730c18e5e2f7fe4
andrea.sangiorgio@gmail.com <andrea.sangiorgio@gmail.com@53f231b7-b343-3494-fe89-795cf1479ec7>
2012-03-24 15:37:58 +0000

    ANOMALIA 24 committ della 1° versione bottone lato utente destinatario review
    step 1): tab Review "Scritta da me", e "Ricevute" ---> terminato al 90%
    step 2): il pulsante __("Add Review")             --->     //    al 50%
    step 3): bottone add review bp admin bar          ---> (non visibile)
    step 4): la lista amici a cui inviare la Review   ---> (in lavorazione)    
    step 5): slug e rewrite (..profile/addreview)     --->     //    al 90%

--- trunk/plugins/bp-review/bp-reviews.php
+++ trunk/plugins/bp-review/bp-reviews.php
@@ -12,8 +12,7 @@
 define('BP_REVIEWS_PLUGIN_DIR',  plugin_dir_path (__FILE__));
 
 class BPUserReviewHelper
-{
-       
+{      
     private static $instance;
     
     public static function get_instance()
@@ -28,82 +27,124 @@ class BPUserReviewHelper
     
     private function __construct() 
 	{        
-        add_action('bp_loaded',array(&$this,'init_component'),0);
-        add_action('init',array(&$this,'register_review_posttype'));     
-        add_action('bp_loaded', array(&$this,'load_textdomain'), 2 );
-        add_action('wp_print_styles',array(&$this,'add_css'));
-        add_action('wp_print_scripts',array(&$this,'add_js'));
+        add_action( 'bp_loaded'		  			, array(&$this , 'init_component'), 0);
+        add_action( 'init'			  			, array(&$this , 'register_review_posttype'));     
+        add_action( 'bp_loaded'		  			, array(&$this , 'load_textdomain'), 2);
+        add_action( 'wp_print_styles'  			, array(&$this , 'add_css'));
+        add_action( 'wp_print_scripts' 			, array(&$this , 'add_js'));		
+		add_action( 'bp_member_header_actions'	, array(&$this , 'add_review_button'));				//[C] 1 - aggiunge il bottone 'Add Review'
+		//add_action( 'init'  					, array(&$this , 'button_modifications', 9);		//[C] 2 - esplorazione HOOKS 
     }
     
     
     function init_component()
 	{
         global $bp;
+
         include_once BP_REVIEWS_PLUGIN_DIR.'core/review-component.php';
+
         $bp->reviews=  BPUserReview::get_instance();
-      }   
+    }   
 
     
-    function is_review_component(){
+    function is_review_component()
+	{
         global $bp;
-        return bp_is_current_component($bp->reviews->root_slug);
+    
+		return bp_is_current_component($bp->reviews->root_slug);
     }
     
+  
+	function register_review_posttype()
+	{
+		//
+		if(is_multisite()&&!is_main_site())
+			return;
+       	  
+		//
+		register_post_type( 'ureviews', array 
+			(
+				'labels' => array(
+					'name' 			=> __( 'User Review','reviews' ),
+					'singular_name' => __( 'User Review','reviews' )
+				)
+				, 'public' 			=> false
+
+				, 'has_archive'   	=> 'reviews' 										//[C] 3 - era 'non presente' 
+				
+				, 'menu_position'	=> 80
+				, 'show_ui'			=> true																							
+				, 'rewrite' 		=> array('slug' => 'ureviews')						// [ST]
+				, 'supports' 		=> array('title','editor','author','comments')   	//OK
+			)
+		);
+	}
    
-   function register_review_posttype()
-   {
-       
-       if(is_multisite()&&!is_main_site())
-           return;
-       
-	   register_post_type( 'ureviews',
-		array(
-			'labels' => array(
-				'name' => __( 'User Review','reviews' ),
-				'singular_name' => __( 'User Review','reviews' )
-			),
-			'public' => false,
-                        'has_archive' => false,
-                        'menu_position'=>80,
-                        'show_ui'=>true,
-			'rewrite' => array('slug' => 'ureviews'),
-                        'supports' => array('title','editor','author','comments')
-		)
-	);
-   }
-   
-function add_css()
-{
-	if(self::is_review_component())
-		wp_enqueue_style ('review',  plugin_dir_url (__FILE__).'/inc/review.css');
-}  
- 
-function add_js()
-{
-	if(self::is_review_component())
-		wp_enqueue_script ('review-js',plugin_dir_url(__FILE__).'inc/review.js',array('jquery'));
-}
+	function add_css()
+	{
+		if(self::is_review_component())
+			wp_enqueue_style ('review',  plugin_dir_url (__FILE__).'/inc/review.css');
+	}  
+	 
+	function add_js()
+	{
+		if(self::is_review_component())
+			wp_enqueue_script ('review-js',plugin_dir_url(__FILE__).'inc/review.js',array('jquery'));
+	}
 
  
- function load_textdomain() 
- {
-	$locale = apply_filters( 'bp_reviews_textdomain_get_locale', get_locale() );
-             	
-	if ( !empty( $locale ) ) 
+	function load_textdomain() 
 	{
-		$mofile_default = sprintf( '%slanguages/%s.mo', plugin_dir_path(__FILE__), $locale );              
-		$mofile = apply_filters( 'bp_reviews_textdomain_mofile', $mofile_default );
-		
-        if ( file_exists( $mofile ) ) 
-		{        
-			load_textdomain( 'reviews', $mofile );
+		$locale = apply_filters( 'bp_reviews_textdomain_get_locale', get_locale() );
+					
+		if ( !empty( $locale ) ) 
+		{
+			$mofile_default = sprintf( '%slanguages/%s.mo', plugin_dir_path(__FILE__), $locale );              
+			$mofile = apply_filters( 'bp_reviews_textdomain_mofile', $mofile_default );
+			
+			if ( file_exists( $mofile ) ) 
+			{        
+				load_textdomain( 'reviews', $mofile );
+			}
 		}
 	}
-}
+
+	/**
+	 * ------------------------------------
+	 * 				[C] 1
+	 * ------------------------------------
+	 * 		
+	 * [I] - Implementa -> CSS per <div e <a
+	 * [T] - Traduci 	-> title, cioè tooltip!
+	 * [T] - Traduci 	-> testo bottone!
+	 *
+	 */
+	function add_review_button()																	
+	{
+		echo '
+			<div style = " float:left;  position:relative;  top:-5px ">
+				<a 	
+					class = "add-reviews button" 
+					title = "Scrivi una Review per l utente." 												
+					href="'.bp_get_displayed_user_link().'reviews/create"
+				>				
+					Add Review
+				</a>
+			</div>';
+	}
+	
+	/**
+	 * ------------------------------------
+	 * 				[C] 2
+	 * ------------------------------------
+	 */
+	function button_modifications() 															
+	{	
+		remove_action('bp_member_header_actions','bp_send_public_message_button');
+	}
 
 }
 
+//
 BPUserReviewHelper::get_instance();
-
-
 ?>
