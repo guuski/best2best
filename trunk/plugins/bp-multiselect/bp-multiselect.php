f296c8cfd2a99850fa6a3ba7fbd66c2d667ffaa9
iofusin@gmail.com <iofusin@gmail.com@53f231b7-b343-3494-fe89-795cf1479ec7>
2012-03-29 11:17:36 +0000

    Plugin sistemato e funzionante.

--- trunk/plugins/bp-multiselect/bp-multiselect.php
+++ trunk/plugins/bp-multiselect/bp-multiselect.php
@@ -36,20 +36,11 @@ add_filter( 'xprofile_field_types', 'bpd_add_new_xprofile_field_type' );
 /*Questo metodo mi consente di interagire con il lato back-end durante la 
  * visualizzazione dei campi profilo  * */
 function bpd_admin_render_new_xprofile_field_type($field, $echo = true){
-		//echo "bpd_admin_render_new_xprofile_field_type<br />";
 		global $ms_nome;
 		ob_start();
 	        switch ( $field->type ) {
 	            case $ms_nome:
-	            
 					ms_getHTMLbackend();
-					
-	                ?>
-
-	                <?php
-						/*
-	                     * <input type="file" name="<?php bp_the_profile_field_input_name() ?>" id="<?php bp_the_profile_field_input_name() ?>" value="" />
-	                     * */
 	                break;   
 	            default :
 	                ?>
@@ -76,7 +67,6 @@ function bpd_admin_render_new_xprofile_field_type($field, $echo = true){
 /*Questo metodo mi consente di interagire con il lato front-end durante la 
  * modifica dei campi profilo  * */	
 function bpd_edit_render_new_xprofile_field($echo = true){
-		//echo "bpd_edit_render_new_xprofile_field<br />";
 		global $ms_nome;
 	    if(empty ($echo)){
 	        $echo = true;
@@ -84,20 +74,7 @@ function bpd_edit_render_new_xprofile_field($echo = true){
 	    
 	    ob_start();
 	        if ( bp_get_the_profile_field_type() == $ms_nome ){
-	            //$imageFieldInputName = bp_get_the_profile_field_input_name();
-	            //$image = WP_CONTENT_URL . bp_get_the_profile_field_edit_value();
-	            
 				 ms_getHTMLfrontend();
-				
-	        ?>
-				
-	        <?php	        
-	        /*
-	         *  <label for="<?php bp_the_profile_field_input_name(); ?>"><?php bp_the_profile_field_name(); ?> <?php if ( bp_get_the_profile_field_is_required() ) : ?><?php _e( '(required)', 'buddypress' ); ?><?php endif; ?></label>
-	            <input type="file" name="<?php echo $imageFieldInputName; ?>" id="<?php echo $imageFieldInputName; ?>" value="" <?php if ( bp_get_the_profile_field_is_required() ) : ?>aria-required="true"<?php endif; ?>/>
-	            <img src="<?php echo $image; ?>" alt="<?php bp_the_profile_field_name(); ?>" />
-	         * */
-	             
 	        }
 	 
 	        $output = ob_get_contents();
@@ -118,7 +95,6 @@ add_action( 'bp_custom_profile_edit_fields', 'bpd_edit_render_new_xprofile_field
 /*Questo metodo riscrive l'action hook di default allo scopo di poter 
  * supportare il nuovo tipo profilo*/
 function bpd_override_xprofile_screen_edit_profile(){
-		//echo "bpd_override_xprofile_screen_edit_profile<br />";
 	    $screen_edit_profile_priority = has_filter('bp_screens', 'xprofile_screen_edit_profile');
 	 
 	    if($screen_edit_profile_priority !== false){
@@ -133,9 +109,7 @@ add_action( 'bp_actions', 'bpd_override_xprofile_screen_edit_profile', 10 );
 	
 //Create profile_edit handler
 function bpd_screen_edit_profile(){
-		//echo "bpd_screen_edit_profile<br />";
 	    if ( isset( $_POST['field_ids'] ) ) {
-			//echo $_POST['field_ids'];
 	        if(wp_verify_nonce( $_POST['_wpnonce'], 'bp_xprofile_edit' )){
 	 
 	            $posted_field_ids = explode( ',', $_POST['field_ids'] );
@@ -150,26 +124,6 @@ function bpd_screen_edit_profile(){
 	 
 	            foreach ( (array)$posted_field_ids as $field_id ) {
 	                $field_name = 'field_' . $field_id;
-	 
-/*
-	                if ( isset( $_FILES[$field_name] ) ) {
-	                    require_once( ABSPATH . '/wp-admin/includes/file.php' );
-	                    $uploaded_file = $_FILES[$field_name]['tmp_name'];
-	 
-	                    // Filter the upload location
-	                    add_filter( 'upload_dir', 'bpd_profile_upload_dir', 10, 1 );
-	 
-	                    //ensure WP accepts the upload job
-	                    $_POST['action'] = 'wp_handle_upload';
-	 
-	                    $uploaded_file = wp_handle_upload( $_FILES[$field_name] );
-	 
-	                    $uploaded_file = str_replace(WP_CONTENT_URL, '', $uploaded_file['url']) ;
-	 
-	                    $_POST[$field_name] = $uploaded_file;
-	 
-	                }
-*/
 	            }
 	 
 	            if($post_action_found){
@@ -188,42 +142,13 @@ function bpd_screen_edit_profile(){
 	        }
 	    }
 	}
-/*	
-			if ( !xprofile_set_field_data( $field_id, $bp->displayed_user->id, $value, $is_required[$field_id] ) )
-					$errors = true;
-				else
-					do_action( 'xprofile_profile_field_data_updated', $field_id, $value );
-			}
-
-			do_action( 'xprofile_updated_profile', $bp->displayed_user->id, $posted_field_ids, $errors );
-*/
-	
-
-/*
-function bpd_profile_upload_dir( $upload_dir ) {
-		//echo "bpd_profile_upload_dir<br />";
-		global $bp;
-	 
-	    $user_id = $bp->displayed_user->id;
-	    $profile_subdir = '/profiles/' . $user_id;
-	 
-	    $upload_dir['path'] = $upload_dir['basedir'] . $profile_subdir;
-	    $upload_dir['url'] = $upload_dir['baseurl'] . $profile_subdir;
-	    $upload_dir['subdir'] = $profile_subdir;
-	 
-		return $upload_dir;
-	}
-*/
 	
 function bpd_load_js() {
-		//echo "bpd_load_js<br />";
 		wp_enqueue_script( 'bpd-js', get_bloginfo('url') . '/wp-content/plugins/bp-multiselect/js/xprofile-multiselect.js',
 							array( 'jquery' ), '1.0' );
-	//	wp_enqueue_script('jquery-ui-widget');
 }
 
 function bpd_load_css() {
-		//echo "bpd_load_css<br />";
 		wp_enqueue_style( 'bpd-css', get_bloginfo('url') . '/wp-content/plugins/bp-multiselect/css/xprofile-multiselect.css');
 }
 
