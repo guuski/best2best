a1d94cce15d9a7b322bfa0e16966bc951688a134
andrea.sangiorgio <andrea.sangiorgio@gmail.com>
2012-03-31 10:07:12 +0000

    NUOVO PLUGIN

---
+++ plugins/bp-r/includes/bp-review-actions.php
@@ -0,0 +1,218 @@
+<?php
+/*
+-----------------------------------------
+Contenuto FILE:
+-----------------------------------------
+
+	
+----------------------------------------------------
+FILE, CLASSI, OGGETTI, METODI collegati o richiamati
+----------------------------------------------------
+		
+	'bp_is_review_component()' 	in 'bp-review-classes()'
+	'bp_get_review_slug()'		in 
+		
+-----------------------------------------
+FUNZIONI e HOOKS (WordPress - Wp)
+-----------------------------------------			
+
+	  
+-----------------------------------------
+FUNZIONI e HOOKS (BuddyPress - Bp)
+-----------------------------------------
+
+	bp_is_current_action()
+	bp_is_action_variable()
+	bp_core_add_message()
+	bp_core_redirect()	
+	bp_is_my_profile()
+	bp_displayed_user_domain()
+	bp_displayed_user_id()
+	bp_loggedin_user_id()
+	
+	
+	'bp_actions' --> 'bp_review_review_save()'
+	
+-----------------------------------------
+global $bp
+-----------------------------------------
+
+-----------------------------------------
+[T]
+-----------------------------------------
+__( 'Review inviata!', 'bp-review' )
+__( 'Review non inviata', 'bp-review' )
+
+
+*/
+
+/**
+ *	NOTA BENE: 
+ *  
+ *  non è richiamata direttamente da NEssuno
+ *  ma viene richiamata perchè è stata aggiunta 
+ *  con 'add_action' all HOOK: 'bp-actions'
+ *
+ */
+function bp_review_review_save() 
+{
+/*
+											//CURRENT ACTION : screen-one
+											//ACTION VARIABLE: send-h5 -->  send-review
+
+
+	// entra se L'utente loggato ha fatto click sul link 'invia review' 		--->		ACTION = 'send-review'
+	if ( bp_is_review_component() && bp_is_current_action( 'screen-one' ) && bp_is_action_variable( 'send-review', 0 ) ) 
+	{
+		//NON CERA E NON SERVE 
+		//---ch--eck_a-dmin_r-eferer( 'new_review', '_wpnonce_new_review' );
+		
+	    //$error	 =	false;
+        //$message =	'';
+		
+		if ( !is_user_logged_in() ) {
+           $message=__('Non sei autorizzato','bp-review');
+           $error=true;
+		}
+		else if ( empty( $_POST['new-review'] ) ) 		//controlla se la variabile è vuota			---// non va!!!
+		{
+			$message = __( 'inserisci del testo', 'bp-review' ) ;
+			$error=true;
+		}
+		
+		if ( bp_is_my_profile() ) 
+		{			
+			bp_core_add_message( __( 'non puoi mandare review a te stesso', 'bp-review' ), 'error' );
+			//$message= __( 'non puoi mandare review a te stesso', 'bp-review' ) ;
+			//$error=true;
+		} 
+		else 
+		{
+			//				chiama la funzione 'bp_review_send_review(utent_1, utente_2)' 		nel FILE 'bp-review-functions.php'	....	se restituisce true è OK!			
+			
+			$result = bp_review_send_review( bp_displayed_user_id(), bp_loggedin_user_id() );
+			
+			//if ( bp_review_send_review( bp_displayed_user_id(), bp_loggedin_user_id() ) ) 
+			if($result)
+			{
+				//$message = __( 'Review inviata correttamente', 'bp-review' ) ;
+				
+				//PROVA var POST!
+					//$message = $_POST['new-review'] . 'ciao';								
+				
+				bp_core_add_message( __( 'Review inviata correttamente', 'bp-review' ) );
+			}
+			else 
+			{
+				bp_core_add_message( __( 'Review non inviata', 'bp-review' ), 'error' );
+				//$message= __( 'Review non inviata...errore!', 'bp-review' ) ;
+				//$error=true;
+			}
+		}
+		  
+		//bp_core_add_message($message,$error);
+		
+		//REDIRECT su screenone
+		bp_core_redirect( bp_displayed_user_domain() . bp_get_review_slug() . '/screen-one' );	//screen one
+		//bp_core_redirect(wp_get_referer()); 												 (---> bp-user-reviews)
+	}
+*/
+}
+
+//IMPORTANTE!
+//add_action( 'bp_actions', 'bp_review_review_save' );
+
+
+function salva() 
+{
+	global $bp;
+	
+	// entra se L'utente loggato ha fatto click sul link 'invia review' 		--->		ACTION = 'send-review'
+	//if ( bp_is_review_component() && bp_is_current_action( 'screen-one' ) && bp_is_action_variable( 'send-review', 0 ) ) 
+		
+	if ( isset( $_POST['review-submit'] ) && bp_is_active( 'review' ) ) 
+	{
+		
+		// [WPNONCE]
+		check_admin_referer( 'bp_review_new_review' );			
+		
+		$content = $_POST['review-content'];		
+			//$content = apply_filters( 'bp_code_snippets_post_snippet_content', $_POST['snippet_content'] );	
+		
+		//DEBUG prova			
+		bp_core_add_message($content);			
+
+		// 
+
+		if ( bp_is_my_profile() ) 
+		{			
+			bp_core_add_message( __( 'non puoi mandare review a te stesso', 'bp-review' ));			
+		} 
+		else 
+		{
+			//AGGIUNTO!
+			if ( empty( $content ) ) 
+			{
+				bp_core_add_message( __( 'Inserisci del testo', 'bp-review' ),'error' );
+				bp_core_redirect( bp_displayed_user_domain() . bp_get_review_slug() . '/screen-one' );	//screen one		
+			}	
+///*	
+			//chiama la funzione 'bp_review_send_review(utent_1, utente_2)' 		nel FILE 'bp-review-functions.php'		....			se restituisce true è OK!									
+			$result = bp_review_send_review( bp_displayed_user_id(), bp_loggedin_user_id(), $content );			
+			
+			if($result)
+			{				
+				bp_core_add_message( __( 'Review inviata correttamente', 'bp-review' ) );
+			}
+			else 
+			{
+				bp_core_add_message( __( 'Review non inviata', 'bp-review' ) );			
+			}
+//*/			
+		}
+
+		//REDIRECT su screenone
+		bp_core_redirect( bp_displayed_user_domain() . bp_get_review_slug() . '/screen-one' );	//screen one		
+	}
+	
+}
+//IMPORTANTE!
+add_action( 'bp_actions', 'salva' );
+
+
+
+//---------------------------------------------------------------
+
+function save_review()
+{
+  /*
+		global $bp;
+     
+		if( 	
+					bp_is_current_component($bp->review->slug)
+				&&  bp_is_current_action('create')
+				&&	!empty($_POST['review-submit'])
+			)														//POST - op 1
+		{
+			// Check the nonce
+            ch--eck_admin_referer( 'new_review', '_wpnonce_new_review' )---;
+			
+			$content = $_POST['new-review'];
+			
+			bp_core_add_message($content);			
+			
+			//redirect
+				//bp_core_redirect(wp_get_referer()); 
+				
+			//REDIRECT su screenone
+			bp_core_redirect( bp_displayed_user_domain() . bp_get_review_slug() . '/screen-one' );	//screen one						
+		}    
+*/		
+}   
+
+//IMPORTANTE!
+
+//add_action('bp_screens','save_review');	
+	//add_action( 'bp_actions', 'save_review' );
+
+?>
