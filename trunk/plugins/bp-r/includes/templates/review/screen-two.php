f67a5e09418c7f6d19d5b5245d8116384a224309
andrea.sangiorgio@gmail.com <andrea.sangiorgio@gmail.com@53f231b7-b343-3494-fe89-795cf1479ec7>
2012-05-02 08:31:23 +0000

    stringhe localizzate

--- trunk/plugins/bp-r/includes/templates/review/screen-two.php
+++ trunk/plugins/bp-r/includes/templates/review/screen-two.php
@@ -70,7 +70,7 @@ get_header() ?>
 				$titolo 			= $_POST['review-title'] 	or '';
 				$contenuto 			= $_POST['review-content']	or '';
 
-				$giudizio_review    = $_POST['giudizio_review']	or 'positivo'; 
+				$giudizio_review    = $_POST['giudizio_review']	or ''; 
 		?>
 				
 		
@@ -93,9 +93,9 @@ get_header() ?>
 				  <input type="radio" name="giudizio_review" id="neutro"   value="neutro"  checked = "<?php checked( $giudizio_review,'neutro') ?>"/>     <label for = "neutro" > Neutra   </label>				  
 				  <input type="radio" name="giudizio_review" id="negativo" value="negativo" checked = "<?php checked( $giudizio_review,'negativo') ?>"/>  <label for = "negativo"> Negativa </label>								 
 -->				  
-  				  <input type="radio" name="giudizio_review" id="positivo" value="positivo"/> <label style="color:green;" for = "positivo" > Positiva </label>				  
-				  <input type="radio" name="giudizio_review" id="neutro"   value="neutro"  />     <label style="color:orange;"  for = "neutro" > Neutra   </label>				  
-				  <input type="radio" name="giudizio_review" id="negativo" value="negativo"/>  <label style="color:red;" for = "negativo"> Negativa </label>								 
+  				  <input type="radio" name="giudizio_review" id="positivo" value="positivo"/> <label style="color:green;" for = "positivo" > <?php _e( 'Positiva', 'reviews' ); ?>  </label>				  
+				  <input type="radio" name="giudizio_review" id="neutro"   value="neutro"  />     <label style="color:orange;"  for = "neutro" > <?php _e( 'Neutra', 'reviews' ); ?>    </label>				  
+				  <input type="radio" name="giudizio_review" id="negativo" value="negativo"/>  <label style="color:red;" for = "negativo"><?php _e( 'Negativa', 'reviews' ); ?>   </label>								 
 				<!-- </fieldset>			-->
 			</div>	
 			
@@ -104,17 +104,17 @@ get_header() ?>
 			<br/>			
 			
 			<div id="new-review-textarea">	
-				<label for="review-title"> Titolo Review </label>			
+				<label for="review-title"> <?php _e( 'Titolo Review', 'reviews' ); ?>  </label>			
 				<textarea name="review-title" id="review-title" cols="2" rows="2"><?php 
 				echo $titolo
 				?></textarea>
-				<span id="nameInfo"> Inserisci un titolo...</span>
+				<span id="nameInfo"> <?php _e( 'Inserisci un titolo...', 'reviews' ); ?>  </span>
 			</div>
 			
 			<br/>
 			
 			<div id="new-review-textarea">			
-				<label for="review-content"> Testo </label>			
+				<label for="review-content">  <?php _e( 'Testo', 'reviews' ); ?>  </label>			
 				<textarea name="review-content" id="review-content" cols="50" rows="10"><?php 
 				echo $contenuto
 				?></textarea>
@@ -122,37 +122,37 @@ get_header() ?>
 
 <br/>	
 
-<div id="new-review-fieldset">		
-	<label for = "review-tipologia-rapporto"> Tipologia Rapporto Commerciale </label>	
+<div id="new-review-tipologia">		
+	<label for = "review-tipologia-rapporto"> <?php _e( 'Tipologia Rapporto Commerciale ', 'reviews' ); ?></label>	
 	<fieldset name = "review-tipologia-rapporto" id = "review-tipologia-rapporto">	  	  
-		<label for = "una tantum"	> <input type="radio" name="tipologia" id="una tantum" value="una tantum"/> Una Tantum  </label> 
-		<label for = "continuativo" > <input type="radio" name="tipologia" id="continuativo" value="continuativo"/>  Continuativo </label>
+		<label for = "una tantum"	> <input type="radio" name="tipologia" id="una tantum" value="una tantum"/> <?php _e( 'Una Tantum ', 'reviews' ); ?> </label> 
+		<label for = "continuativo" > <input type="radio" name="tipologia" id="continuativo" value="continuativo"/> <?php _e( 'Continuativo', 'reviews' ); ?>  </label>
 	</fieldset>			
 </div>
 
 <br/>	
 
 <!-- <div id="new-review-input">		-->
-	<label for = "datepicker"> Data Inizio Rapporto Comm. <input type="text" name ="datepicker" id="datepicker" maxlength="12" size="20"> </label>		
+	<label for = "datepicker"> <?php _e( 'Data Inizio Rapporto Comm. ', 'reviews' ); ?><input type="text" name ="datepicker" id="datepicker" maxlength="12" size="20"> </label>		
 	<!-- </div>-->
 <!-- </div>-->
 
 <br/>	
 <br/>	
 
-<div id="new-review-fieldset">		
-	<label for = "utente_consigliato"> Lo raccomanderesti?</label>	
+<div id="new-review-consigliato">		
+	<label for = "utente_consigliato"> <?php _e( 'Lo raccomanderesti?', 'reviews' ); ?></label>	
 	<fieldset name = "utente_consigliato" id = "utente_consigliato">	  	  
-		<label for = "si"> <input type="radio" name="consigliato" id="si" value="si"/>  Si </label> 	 	
-		<label for = "no"> <input type="radio" name="consigliato" id="no" value="no"/>  No </label> 	 	  
-		<label for = "nonso"> <input type="radio" name="consigliato" id="nonso" value="nonso"/> Non so </label> 
+		<label for = "si"> <input type="radio" name="consigliato" id="si" value="si"/><?php _e( 'Si', 'reviews' ); ?>   </label> 	 	
+		<label for = "no"> <input type="radio" name="consigliato" id="no" value="no"/>  <?php _e( 'No', 'reviews' ); ?> </label> 	 	  
+		<label for = "nonso"> <input type="radio" name="consigliato" id="nonso" value="nonso"/> <?php _e( 'Non so', 'reviews' ); ?> </label> 
 	</fieldset>			
 </div>
 	
 <!--------------------------------------------- sezione RATING ------------------------------------->
 		<div id="new-review-rating">			
 					
-			<div class="rating-container"><span class="rating-title">Prezzo</span> 
+			<div class="rating-container"><span class="rating-title"><?php _e( 'Prezzo', 'reviews' ); ?></span> 
 				<ul id="prezzo" class='star-rating'>	
 					<li class='current-rating' style="width: <?php echo 25*$prezzo;?>px"></li>			
 					<li><a href="#" onclick="return vote(1, this);" title='1 / 5' class='one-star'>1</a></li>
@@ -163,7 +163,7 @@ get_header() ?>
 				</ul><input type="hidden" name="prezzo" value="<?php echo $prezzo?>" />
 			</div>		
 			
-			<div class="rating-container"><span class="rating-title">Servizio</span> <ul id="servizio" class='star-rating'>	
+			<div class="rating-container"><span class="rating-title"><?php _e( 'Servizio', 'reviews' ); ?></span> <ul id="servizio" class='star-rating'>	
 				<li class='current-rating' style="width: <?php echo 25*$servizio;?>px"></li>			
 				<li><a href="#" onclick="return vote(1, this);" title='1 / 5' class='one-star'>1</a></li>
 				<li><a href="#" onclick="return vote(2, this);" title='2 / 5' class='two-stars'>2</a></li>
@@ -172,7 +172,7 @@ get_header() ?>
 				<li><a href="#" onclick="return vote(5, this);" title='5 / 5' class='five-stars'>5</a></li>
 			</ul><input type="hidden" name="servizio" value="<?php echo $servizio?>" />
 			</div>	
-			<div class="rating-container"><span class="rating-title">Qualit&agrave;</span> <ul id="qualita" class='star-rating'>	
+			<div class="rating-container"><span class="rating-title"><?php _e( 'Qualit&agrave;', 'reviews' ); ?></span> <ul id="qualita" class='star-rating'>	
 				<li class='current-rating' style="width: <?php echo 25*$qualita;?>px"></li>			
 				<li><a href="#" onclick="return vote(1, this);" title='1 / 5' class='one-star'>1</a></li>
 				<li><a href="#" onclick="return vote(2, this);" title='2 / 5' class='two-stars'>2</a></li>
@@ -181,7 +181,7 @@ get_header() ?>
 				<li><a href="#" onclick="return vote(5, this);" title='5 / 5' class='five-stars'>5</a></li>
 			</ul><input type="hidden" name="qualita" value="<?php echo $qualita?>" />
 			</div>		
-			<div class="rating-container"><span class="rating-title">Puntualit&agrave;</span> <ul id="puntualita" class='star-rating'>	
+			<div class="rating-container"><span class="rating-title"><?php _e( 'Puntualit&agrave;', 'reviews' ); ?></span> <ul id="puntualita" class='star-rating'>	
 				<li class='current-rating' style="width: <?php echo 25*$puntualita;?>px"></li>			
 				<li><a href="#" onclick="return vote(1, this);" title='1 / 5' class='one-star'>1</a></li>
 				<li><a href="#" onclick="return vote(2, this);" title='2 / 5' class='two-stars'>2</a></li>
@@ -190,7 +190,7 @@ get_header() ?>
 				<li><a href="#" onclick="return vote(5, this);" title='5 / 5' class='five-stars'>5</a></li>
 			</ul><input type="hidden" name="puntualita" value="<?php echo $puntualita?>" />
 			</div>	
-			<div class="rating-container"><span class="rating-title">Affidabilit&agrave;</span> <ul id="affidabilita" class='star-rating'>	
+			<div class="rating-container"><span class="rating-title"> <?php _e( 'Affidabilit&agrave;', 'reviews' ); ?></span> <ul id="affidabilita" class='star-rating'>	
 				<li class='current-rating' style="width: <?php echo 25*$affidabilita;?>px"></li>			
 				<li><a href="#" onclick="return vote(1, this);" title='1 / 5' class='one-star'>1</a></li>
 				<li><a href="#" onclick="return vote(2, this);" title='2 / 5' class='two-stars'>2</a></li>
@@ -205,10 +205,10 @@ get_header() ?>
 
 <br />
 		
-<div id="new-review-fieldset">		
-	<label for = "disclaimer"> Disclaimer, Termini e Condizioni</label>	
+<div id="new-review-disclaimer">		
+	<label for = "disclaimer"> <?php _e( 'Disclaimer, Termini e Condizioni', 'reviews' ); ?></label>	
 	<fieldset name = "disclaimer" id = "disclaimer">	  
-		<label for = "disclaimer"> Accetto </label> <input type="checkbox" name="disclaimer" value="si"/>	  
+		<label for = "disclaimer"> <?php _e( 'Accetto', 'reviews' ); ?></label> <input type="checkbox" name="disclaimer" value="si"/>	  
 	</fieldset>			
 </div>
 
