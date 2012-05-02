2b63c00891387ece5405a3ced1f6881bd1ff46bb
iofusin@gmail.com <iofusin@gmail.com@53f231b7-b343-3494-fe89-795cf1479ec7>
2012-05-02 09:02:24 +0000

    ho messo ogni widget in uno specifico file e ho inserito __( per la localizzazione di ogni widget ed ho modificato messaggiadmin.php per poter mandare anche html e scrivere il messaggio in una textarea e non più in un input type text.

---
+++ trunk/plugins/bp-custom/completaprofilo.php
@@ -0,0 +1,357 @@
+<?php
+
+class completaProfilo_Widget extends WP_Widget {
+	/**
+	 * Constructor
+	 * 
+	 * Registers the widget details with the parent class
+	 */
+	var $fr_type=null;
+	
+	function completaProfilo_Widget(){
+		
+		$id_base = 'completaProfilo_Widget';
+		
+		$name = __('Completa Profilo',"custom");
+		
+		$widget_options = array( 'titolo' => __('Completamento Profilo '),"custom");
+		
+		$control_ops = '';
+		
+		parent::WP_Widget($id_base, $name, $widget_options, $control_ops);
+		
+	}
+	
+
+	function form($instance){
+		
+		// outputs the options form on admin
+		
+		$instance = wp_parse_args( (array) $instance, array(  'titolo' => __('Completamento Profilo ' ),"custom"));
+		
+		$titolo = __(strip_tags( $instance['titolo'] ),"custom");
+		?>
+			<label>Titolo <input id="<?php echo $this->get_field_id( 'titolo' ); ?>" name="<?php echo $this->get_field_name( 'titolo' ); ?>" type="text" value="<?php echo esc_attr( $titolo ); ?>"  style="width: 70%; float:right;"  /></label>
+		<?php 
+	}
+
+	function update($new_instance, $old_instance){
+		
+		$instance = $old_instance;
+		
+		$instance['titolo'] = __(strip_tags( $new_instance['titolo'] ),"custom");
+		
+		return $instance;
+	}
+	
+	function get_current_field($id){
+		
+		global $bp;
+		
+		global $wpdb;
+	
+		$query = "SELECT d.field_id, d.value FROM wp_bp_xprofile_data d WHERE d.user_id=$id";
+		
+		$ms_output= $wpdb->get_results( $wpdb->prepare($query));
+		
+		return $ms_output;
+	}
+	
+	//effettua la query per caricare tutti i tipi dei vari utenti e li inserisce dentro la variabile fr_type
+	function globalType(){
+		
+		global $bp;
+		
+		global $wpdb;
+	
+		$query = "SELECT d.user_id, d.value FROM wp_bp_xprofile_data d WHERE d.value='Albergo/Ristorante' OR d.value='Fornitore' OR d.value='Utente'";
+		
+		$ms_output= $wpdb->get_results( $wpdb->prepare($query));
+		
+		$this->fr_type=$ms_output;
+	}
+	
+	//ritorna il tipo (Albergo/Ristorante o Fornitore) dell'utente avente id =$ID
+	function get_type($ID){
+		
+		if ($this->fr_type==null)
+		
+			 $this->globalType();
+			 
+		foreach ( (array)$this->fr_type as $k){
+			
+			if ($k->user_id==$ID)
+			
+				return __($k->value,"custom");
+		}
+		
+		return __("Non riconosciuto","custom");
+		
+	}
+	
+	function get_total_field(){
+		
+		global $bp;
+		
+		global $wpdb;
+		
+		global $user_ID;
+	
+		$query = "SELECT f.id , f.group_id, f.name FROM wp_bp_xprofile_fields f WHERE parent_id=0";
+		
+		$ms_output= $wpdb->get_results( $wpdb->prepare($query));
+		
+		$user_info = get_userdata($user_ID);
+		
+		//$user_type=$this->get_type($user_info->ID);
+		
+		$user_type=BP_XProfile_ProfileData::get_value_byfieldname("Tipo profilo");
+		
+		$output=array();
+		
+		$count=0;
+		
+		foreach ($ms_output as $k =>$value){
+			
+			if ($value->name=='Categoria' ||
+				$value->name=='Categoria attivita&#39;' ||
+				$value->name=='Lista aree di copertura' ||
+				$value->name=='Descrivi prodotti / servizi' ||
+				$value->name=='Macro categoria attività') {
+					
+					if ($user_type=='Fornitore'){
+						$output[$count]=$value;
+						$count++;
+						}
+						
+			} else if (	$value->name=='Numero letti / coperti' || 
+						$value->name=='Numero stelle' ) {
+							if ($user_type=='Albergo/Ristorante' ){
+								$output[$count]=$value;
+								$count++;
+							}
+			} else {
+				$output[$count]=$value;
+				$count++;
+				}
+		}
+		return $output;
+	}
+	function adatta($tot,$parz){
+		
+		foreach ($parz as $k => $v){
+			$trovato=false;
+			foreach ($tot as $k1 =>$v1){
+				
+				if ($v->field_id==$v1->id && $v->value!='' && $v->value!='a:0:{}'){
+					$trovato=true;
+					//echo  "#".$v->value."#<br />";
+				}
+			}
+			
+			if (!$trovato){
+				unset($parz[$k]);
+				
+				}
+			}
+		return $parz;
+	}
+	function widget($args, $instance)
+	{
+		global $bp;
+		global $user_ID;
+		
+		if ($user_ID==0) {
+			//non visualizzo nulla
+			}
+		else
+		{
+		echo $this->ms_getScript();//carico gli script
+		
+		$attivo=array();
+		extract($args);
+		$title = apply_filters('widget_title', empty($instance['titolo']) ? __('Completamento Profilo ','custom') : $instance['titolo'], $instance, $this->id_base);
+		
+		//analizzo il profilo
+		$current = $this->get_current_field($user_ID);
+		$total = $this->get_total_field();
+		$current = $this->adatta($total,$current);
+		
+		$cPar=count($current);
+		$cTot=count($total);
+		$profilo="";
+		foreach($total as $k => $v) {
+			$trovato=false;
+			$visibile=false;
+			
+			foreach($current as $kcur => $vcur) {
+				if ($vcur->field_id==$v->id) $trovato=true;
+			}
+			
+			if ($trovato)
+				$profilo.= "<b style='text-decoration: line-through;'>".__($v->name,'custom')."</b><br />";
+			else
+				$profilo.= __($v->name,'custom')."<br />";
+			//$cTot++;
+		}
+		
+		
+		//if ( $title )
+			//echo $before_title . $title .(int)(100*$cPar/$cTot)."% ". $after_title;
+			
+		if ($cPar>=$cTot) { 
+			$perc=(int)(100*$cPar/$cTot);
+			echo "		<div class='box_percentuale_profilo'
+								onmouseover='ms_labelprofiloon(this)' 
+								onmouseout='ms_labelprofilooff(this)'
+								onclick='ms_openprofilo(\"completaprofilo\")'>
+								
+								<div class='box_percentuale_profilo_perc' style='width:$perc%;'
+								onmouseover='ms_labelprofiloon(this)' 
+								onmouseout='ms_labelprofilooff(this)'></div> 
+								
+								<label value='chiuso' 
+								onmouseover='ms_labelprofiloon(this)' 
+								onmouseout='ms_labelprofilooff(this)'>			
+									".__("Profilo Completo","custom") .$perc."% 
+								</label>
+							
+						</div>
+							
+							<div id=\"completaprofilo\" class='box_completa_profilo' style='display:none;'>
+								$profilo
+							</div>";
+		}
+		else {
+			//completamento Registrazione===================================
+			$perc=(int)(100*$cPar/$cTot);
+			echo "		
+			
+						Migliora la tua visibilità:<br />
+						<a href='".bp_loggedin_user_domain()."profile/edit/group' style='width:80%;'>
+							".__("Completa il tuo profilo!","custom")."
+						</a>
+						<div class='box_percentuale_profilo'
+								onmouseover='ms_labelprofiloon(this)' 
+								onmouseout='ms_labelprofilooff(this)'
+								onclick='ms_openprofilo(\"completaprofilo\")'>
+								
+								<div class='box_percentuale_profilo_perc' style='width:$perc%;'
+								onmouseover='ms_labelprofiloon(this)' 
+								onmouseout='ms_labelprofilooff(this)'></div> 
+								
+								<label value='chiuso' 
+								onmouseover='ms_labelprofiloon(this)' 
+								onmouseout='ms_labelprofilooff(this)'>			
+									".__("Completamento Profilo ","custom").$perc."% 
+								</label>
+							
+						</div>
+							
+							<div id=\"completaprofilo\" class='box_completa_profilo' style='display:none;'>
+								$profilo
+							</div>
+					
+					";
+							
+		
+		}
+			//==============================================================
+		?>
+		
+	
+		
+		<?php 
+	
+		}
+
+		
+	}
+	
+	function ms_getScript(){ ?>
+	
+	<script language='JavaScript' type='text/javascript'>
+	<!--
+
+		function ms_openprofilo(divname)
+		{
+			if (jQuery('div#'+divname).val()=="aperto") {
+				jQuery('div#'+divname).hide("slow");	
+				jQuery('div#'+divname).val("chiuso");
+				//jQuery('div#'+divname).css("color","red");
+			}
+			else{
+				jQuery('div#'+divname).val("aperto");
+				jQuery('div#'+divname).show("slow");
+			}
+		}
+		function ms_labelprofiloon(t)
+		{	
+			t.style.cursor = 'pointer';
+		}
+
+		function ms_labelprofilooff(t)
+		{
+			t.style.cursor = 'default';
+		}	
+	//-->
+	</script>
+	
+	<script>
+	<!--
+		function ms_labelon(t)
+		{	
+			t.style.color = '#ffffff';
+			t.style.background ='#eaeaea' ;
+			t.style.cursor = 'pointer';
+		}
+
+		function ms_labeloff(t)
+		{
+			t.style.color = 'rgb(68,68,68)';
+			t.style.background ='#ffffff' ;
+			t.style.cursor = 'default';
+		}	
+	//-->	
+	</script>
+	
+	<style type='text/css'>
+		.box_percentuale_profilo{
+			position:relative;
+			background-color:#87badd;
+			color:rgb(68,68,68);
+			border:2px solid #ffffff;
+			font-weight:bold;
+			height:20px;
+			}
+		.box_percentuale_profilo label{
+			position:relative;
+			margin: 0px auto;
+			margin-left:10px;
+			color:#ffffff;
+			}
+			
+		.box_percentuale_profilo_perc{
+			position:absolute;
+			top:0px; left:0px; bottom:0px;
+			margin:0px; 
+			padding:0px; 
+			height:100%; 
+			background-color:#0a76b7;
+			//border-right: 1px solid #555555;
+		}
+		.box_completa_profilo{
+			border:2px solid #ffffff;
+			border-top:0px;
+			padding-left:4px;
+			background-color:white;
+		}
+	</style>
+
+	<?php
+	
+	}
+
+}
+
+?>
---
+++ trunk/plugins/bp-custom/fornitorialberghi.php
@@ -0,0 +1,146 @@
+<?php
+
+class FornitoriAlberghi_Widget extends WP_Widget {
+	/**
+	 * Constructor
+	 * 
+	 * Registers the widget details with the parent class
+	 */
+	var $fr_type=null;
+	function FornitoriAlberghi_Widget(){
+		//parent::WP_Widget( $id = 'bpcustom_Widget', $name = 'Amici', $options = array( 'titolo' => 'Amici', 'numero' =>3 ));
+		parent::WP_Widget( $id = 'FornitoriAlberghi_Widget', $name = __('Fornitori Alberghi','custom'));
+	}
+	
+
+	function form($instance){
+
+		echo __("Questo widget se l'utente è un fornitore, visualizza la lista degli alberghi suoi amici, se invece è un albergo, visualizza la lista dei fornitori","custom");
+			
+
+		// outputs the options form on admin
+/*
+		?>
+		<p><label>Titolo		<input id="<?php echo $this->get_field_id( 'titolo' ); ?>" name="<?php echo $this->get_field_name( 'titolo' ); ?>" type="text" value="<?php echo esc_attr( $titolo ); ?>" style="width: 30%" /></label></p>
+		<p><label>Numero Amici 	<input id="<?php echo $this->get_field_id( 'numero' ); ?>" name="<?php echo $this->get_field_name( 'numero' ); ?>" type="text" value="<?php echo esc_attr( $numero ); ?>" style="width: 30%" /></label></p>
+		
+		<?php 
+*/
+	}
+
+	function update($new_instance, $old_instance){
+		$instance = $old_instance;
+		//$instance['titolo'] = strip_tags( $new_instance['titolo'] );
+		//$instance['numero'] = strip_tags( $new_instance['numero'] );
+		return $instance;
+	}
+
+	function widget($args, $instance){
+		global $bp;
+		global $user_ID;
+		$fullname=$bp->loggedin_user->fullname;
+		$user = $bp->displayed_user->id;
+		
+		if ($user!=0) {
+			$user_ID=$user;
+			$fullname=bp_get_displayed_user_fullname();
+		}
+		
+		if ($user_ID==0) {
+			//visualizzo 8 utenti (amici dell'amministatore)
+			$listfriend = friends_get_friend_user_ids(1);
+			$cont=0;
+			
+			echo "<div>";
+			foreach ($listfriend as $k => $v){
+				$attivo = get_userdata($v);
+				
+				if ($cont<8){
+					echo  "<a href='".get_bloginfo('url').DS."registrati/' >".get_avatar($v,42)."</a>";
+					$cont++;
+				}
+			}
+			echo "</div>";
+			//non visualizzo nulla
+			}
+		else
+		{
+		
+		$attivo=array();
+		extract($args);
+		$user_info = get_userdata($user_ID);
+		
+		$user_type=$this->get_type($user_info->ID);
+		if ($user_type=="Fornitore" || $user_type=="Albergo/Ristorante" || $user_type=="Utente")
+		{
+			if ($user_type=="Fornitore")
+				$title = apply_filters('widget_title', empty($instance['titolo']) ? __('I clienti di ','custom').$fullname: $instance['titolo'], $instance, $this->id_base);
+			else if ($user_type=="Albergo/Ristorante")
+				$title = apply_filters('widget_title', empty($instance['titolo']) ? __('I fornitori di ','custom').$fullname: $instance['titolo'], $instance, $this->id_base);
+			else if ($user_type=="Utente")
+				$title = apply_filters('widget_title', empty($instance['titolo']) ? __('Gli amici di ','custom').$fullname: $instance['titolo'], $instance, $this->id_base);
+				
+			// outputs the content of the widget
+		
+
+		//echo $before_widget;
+		if ( $title )
+			echo $before_title . $title . $after_title.'<div class="avatar-block">';
+
+		
+			$listfriend = friends_get_friend_user_ids($user_ID);
+			
+			foreach ($listfriend as $k => $v){
+				$attivo = get_userdata($v);		
+					if ($user_type=="Fornitore" && $this->get_type($v)=="Albergo/Ristorante"){
+						echo  "<a href='".bp_core_get_user_domain($attivo->user_login).$attivo->user_login."' >".get_avatar($v,42)."</a>";	
+					}
+					else if ($user_type=="Albergo/Ristorante" && $this->get_type($v)=="Fornitore"){
+						echo  "<a href='".bp_core_get_user_domain($attivo->user_login).$attivo->user_login."' >".get_avatar($v,42)."</a>";
+					}
+					else if ($user_type=="Utente"){
+						echo  "<a href='".bp_core_get_user_domain($attivo->user_login).$attivo->user_login."' >".get_avatar($v,42)."</a>";
+					}					
+			}
+			echo "</div>";
+
+			
+			?>
+		
+	
+			
+
+
+			<?php 
+		}
+		}
+	}
+	
+	//effettua la query per caricare tutti i tipi dei vari utenti
+	function globalType(){
+		global $bp;
+		global $wpdb;
+	
+		$query = "SELECT d.user_id, d.value FROM wp_bp_xprofile_data d WHERE d.value='Albergo/Ristorante' OR d.value='Fornitore' OR d.value='Utente'";
+		$ms_output= $wpdb->get_results( $wpdb->prepare($query));
+		$this->fr_type=$ms_output;
+		
+	}
+	//ritorna il tipo (Albergo/Ristorante o Fornitore) dell'utente avente id =$ID
+	function get_type($ID){
+		if ($this->fr_type==null)
+			 $this->globalType();
+			 
+		
+		
+		foreach ( (array)$this->fr_type as $k){
+			if ($k->user_id==$ID)
+				return $k->value;
+		}
+		return __('Non riconosciuto','custom');
+	}
+}
+//INSERT INTO wp_bp_xprofile_fields ( group_id, parent_id, type, name , description , is_required , is_default_option, field_order, option_order , order_by, can_delete) VALUES (1,2,'option','Utente','',0,1,0,1,'',1)
+
+
+?>
