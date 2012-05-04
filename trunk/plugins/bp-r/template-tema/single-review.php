30327f2a23358c9dd4649e5eb5498ffc964287ab
andrea.sangiorgio@gmail.com <andrea.sangiorgio@gmail.com@53f231b7-b343-3494-fe89-795cf1479ec7>
2012-05-04 17:47:17 +0000

    BUG visualizzazione COMMENTI nella pagina della singola REVIEW corretto parzialmente (mostra una lista semplice!)

--- trunk/plugins/bp-r/template-tema/single-review.php
+++ trunk/plugins/bp-r/template-tema/single-review.php
@@ -9,6 +9,8 @@
 
 			<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
 
+			
+			
 				<div id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 
 <!-- DESTINATARIO BOX -->			
@@ -53,8 +55,20 @@
 						<div class="entry">
 							<?php the_content( __( 'Read the rest of this entry &rarr;', 'buddypress' ) ); ?>
 
+							
+			
+
+							
+							
 							<?php wp_link_pages( array( 'before' => '<div class="page-link"><p>' . __( 'Pages: ', 'buddypress' ), 'after' => '</p></div>', 'next_or_number' => 'number' ) ); ?>
+							
+							
+							<?php //wp_list_comments();							 ?>
+							
+							
 						</div>
+						
+						
 <?php 	
 				$prezzo = get_post_meta( $post->ID, 'voto_prezzo', true );		
 				$servizio = get_post_meta( $post->ID, 'voto_servizio', true );
@@ -126,16 +140,11 @@
 			<!-- <div id='current-rating-result'></div>  used to show "success" message after vote -->
 					  
 		</div>	<!-- fine sezione RATING -->		
+
+
 		
-						<p class="postmetadata"><?php the_tags( '<span class="tags">' . __( 'Tags: ', 'buddypress' ), ', ', '</span>' ); ?>&nbsp;</p>
+<br/> <br/>
 
-						<div class="alignleft"><?php //previous_post_link( '%link', '<span class="meta-nav">' . _x( '&larr;', 'Previous post link', 'buddypress' ) . '</span> %title' ); ?></div>
-						<div class="alignright"><?php //next_post_link( '%link', '%title <span class="meta-nav">' . _x( '&rarr;', 'Next post link', 'buddypress' ) . '</span>' ); ?></div>
-					</div>
-
-				</div>
-				
-				
 <!-- FORM per COMMENTI -->
 
 	<?php
@@ -148,13 +157,219 @@
 		{
 			comments_template();
 		}		
+		else 
+		{
+		
+			//echo 'dovresti vedere i commenti qui!'; 
+			
+			
+//---------------------------------------------------------------------------------------------------//---------------------------------------------------------------------------------------------------								
+			
+			
+			$post_id = $post->ID;
+			//$post_id_2 = the_ID();
+			
+			
+			
+			$pre_HTML ="";
+			$post_HTML ="";
+
+			global $wpdb;
+/*
+			$sql = "SELECT DISTINCT ID, post_title, post_password, comment_ID, comment_post_ID, comment_author, comment_date_gmt, comment_approved, comment_type,comment_author_url, SUBSTRING(comment_content,1,30) AS com_excerpt FROM $wpdb->comments LEFT OUTER JOIN $wpdb->posts ON ($wpdb->comments.comment_post_ID = $wpdb->posts.ID)
+			WHERE comment_approved = '1' 	
+			AND comment_type = '' AND post_password = '' ORDER BY comment_date_gmt DESC LIMIT 10";
+*/			
+		//AND ID = '1'
+		
+			$sql = 				
+				"SELECT DISTINCT ID, post_title, post_password, comment_ID, comment_post_ID, comment_author, comment_date_gmt, comment_approved, comment_type,comment_author_url, SUBSTRING(comment_content,1,30) AS com_excerpt 
+			
+				FROM $wpdb->comments LEFT OUTER JOIN $wpdb->posts ON ($wpdb->comments.comment_post_ID = $wpdb->posts.ID)
+			
+				WHERE comment_approved = '1' 				
+				AND comment_type = '' 
+				AND post_password = '' 
+			
+				AND post_type='review'
+				AND ID='262'
+			
+				ORDER BY comment_date_gmt DESC LIMIT 10
+			";
+			
+/*			
+			$postids = $wpdb->get_col( 
+			$wpdb->prepare( 
+	"
+	SELECT      key1.post_id
+	FROM        $wpdb->postmeta key1
+	INNER JOIN  $wpdb->postmeta key2
+	            ON key2.post_id = key1.post_id
+	            AND key2.meta_key = %s
+	WHERE       key1.meta_key = %s
+	ORDER BY    key2.meta_value+(0) ASC
+	",
+        $meta_key2,
+	$meta_key1
+) ); 
+			
+*/
+
+			//$comments = $wpdb->get_results($sql);
+			
+		$comments = 
+			$wpdb->get_results( 
+				$wpdb->prepare( 
+					"SELECT DISTINCT ID, post_title, post_password, comment_ID, comment_post_ID, comment_author, comment_date_gmt, comment_approved, comment_type,comment_author_url, SUBSTRING(comment_content,1,30) AS com_excerpt 
+				
+					FROM $wpdb->comments LEFT OUTER JOIN $wpdb->posts ON ($wpdb->comments.comment_post_ID = $wpdb->posts.ID)
+				
+					WHERE comment_approved = '1' 				
+					AND comment_type = '' 
+					AND post_password = '' 
+				
+					AND post_type='review'
+					AND ID=%s
+				
+					ORDER BY comment_date_gmt DESC LIMIT 10
+					
+					",
+					$post_id
+				)
+			);
+			
+			
+			
+			
+			
+			$output = $pre_HTML;
+			
+				$output .= '<div id="comments">';
+			
+			//$output .= "\n<ul>";
+				$output .= '<ol class = "commentlist">';
+
+			foreach ($comments as $comment) 
+			{
+				$output .= 
+				
+				//"\n<li>" 
+				'<div class="comment-body" >'
+				
+				.	strip_tags($comment->comment_author) .":" 
+				.   "<a href=\"" . get_permalink($comment->ID)
+				.   "#comment-" . $comment->comment_ID 
+				. 	"\" title=\"on ".$comment->post_title 
+				.   "\">" . strip_tags($comment->com_excerpt)
+				.   "</a>"
+				
+					.	"</div>";
+				//.	"</li>";
+				
+			}
+
+			//$output .= "\n</ul>";
+				$output .= "</ol>";
+				$output .= "</div>";
+			
+			$output .= "\n\n post_ID:   " . $post_id . "  post_ID 2:   " . $post_id_2;
+			
+			
+			$output .= $post_HTML;
+	
+			echo $output;
+
+//---------------------------------------------------------------------------------------------------
+  
+  
+			$args = array(
+				'walker'            => null,
+				'max_depth'         => 5,
+				'style'             => 'ul',
+				//'callback'          => null,
+												'callback'          => mytheme_comment,
+				'end-callback'      => null,
+				'type'              => 'all',
+/*				
+				'page'              => ,
+				'per_page'          => ,
+*/				
+				'avatar_size'       => 32,
+				'reverse_top_level' => null
+				//,
+				//'reverse_children'  =>  
+			); 
+				
+			//echo '<ol class="commentlist">';
+				//wp_list_comments('type=comment');			
+			//echo '</ol>';
+			
+				//wp_list_comments($args);
+			
+			//<ul class="commentlist">
+				//wp_list_comments('type=comment&callback=mytheme_comment'); 
+				//wp_list_comments('type=comment&callback=mytheme_comment_2'); 
+			//</ul>
+			
+			
+			
+			//comments_template('/frisco-comments.php');
+		}
 				
 	?>		
 <!-- fine FORM per COMMENTI -->			
 			
 			
 			
+
+		
+		
+		
+		
+		
+		
+		
+		
+						<p class="postmetadata"><?php the_tags( '<span class="tags">' . __( 'Tags: ', 'buddypress' ), ', ', '</span>' ); ?>&nbsp;</p>
+
+						<div class="alignleft"><?php //previous_post_link( '%link', '<span class="meta-nav">' . _x( '&larr;', 'Previous post link', 'buddypress' ) . '</span> %title' ); ?></div>
+						<div class="alignright"><?php //next_post_link( '%link', '%title <span class="meta-nav">' . _x( '&rarr;', 'Next post link', 'buddypress' ) . '</span>' ); ?></div>
+						
+						
+<!--	
+						<?php if($comments) : ?>  
+    <?php foreach($comments as $comment) : ?>  
+        <?php if ($comment->comment_approved == '0') : ?>  
+        <?php endif; ?>  
+    <?php endforeach; ?>  
+<?php else : ?>  
+	<?php echo 'no commenti';?>
+<?php endif; ?>  
+-->
+		
+		<?php //wp_list_comments();?>
+		
+		
+		
+		
+		
+		
+		
+</div>
+
+</div>
+				
+				
+
 			
+<?php
+
+ 
+
+?>
+			
+			
+
 
 			<?php endwhile; else: ?>
 
@@ -162,13 +377,12 @@
 
 			<?php endif; ?>
 
-		</div>
 
-		<?php do_action( 'bp_after_blog_single_post' ) ?>
+		
+</div> <!-- page -->
+<?php do_action( 'bp_after_blog_single_post' ) ?>
+</div><!-- .padder -->
+</div><!-- #content -->
 
-		</div><!-- .padder -->
-	</div><!-- #content -->
-
-	<?php get_sidebar() ?>
-
+<?php get_sidebar() ?>
 <?php get_footer() ?>
