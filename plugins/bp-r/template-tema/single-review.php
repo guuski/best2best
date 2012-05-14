853b2e45cbdbdd555c2b6487bd02a8e4551946bc
giambap <giambap@gmail.com>
2012-05-14 16:23:12 +0000

    fix aspetto

--- plugins/bp-r/template-tema/single-review.php
+++ plugins/bp-r/template-tema/single-review.php
@@ -17,13 +17,17 @@
 <div class="author-box"> 														<!-- NOTA BENE: continua a chiamarsi AUTHOR BOX! -->
 	
 	<?php
-		$destinatario_review_id = get_post_meta( $post->ID, 'bp_review_recipient_id', true ); 
+		
+		$destinatario_review_id = get_post_meta( $post->ID, 'bp_review_recipient_id', true );
+		$points = get_the_author_meta('media_voto_review',$destinatario_review_id);
 		$nome = xprofile_get_field_data( "Nome" , $destinatario_review_id);
 	?>					
 		
 	<?php echo get_avatar( $destinatario_review_id  , '70' ); //echo $destinatario_review_id  ?>		
 	<p>	
 		<?php printf( _x( 'Recensione su %s', 'Recensione su...', 'reviews' ), bp_core_get_userlink(  $destinatario_review_id ) ) ?>
+		<?php if($points != '' ) : ?>(<?php echo __('Media','reviews').": "; printf("%.2d", $points) ?>)
+		<?php endif;?>  
 	</p>		
 	
 </div> <!-- fine BOX -->		
@@ -44,21 +48,7 @@
 		<?php echo $nome; //the_author_meta('user_nicename');?>
 	</a>
 </strong> </small> <!-- fine BOX -->									
-						
-<!-- -->						
-<h2 class="posttitle"><?php the_title(); ?>	</h2>
-
-	<p class="date">
-		<?php printf( __( '%1$s <span>in %2$s</span>', 'buddypress' ), get_the_date(), get_the_category_list( ', ' ) ); ?>
-		<span class="post-utility alignright"><?php edit_post_link( __( 'Edit this entry', 'buddypress' ) ); ?></span>
-	</p>
-
-	<div class="entry">
-		<?php the_content( __( 'Read the rest of this entry &rarr;', 'buddypress' ) ); ?>
-		<?php wp_link_pages( array( 'before' => '<div class="page-link"><p>' . __( 'Pages: ', 'buddypress' ), 'after' => '</p></div>', 'next_or_number' => 'number' ) ); ?>			
-	</div>
-	
-	<?php 	
+			<?php 	
 	
 		$prezzo 			= get_post_meta( $post->ID, 'voto_prezzo', true );		
 		$servizio 			= get_post_meta( $post->ID, 'voto_servizio', true );
@@ -69,7 +59,7 @@
 		$data_rapporto 		= get_post_meta( $post->ID, 'data_rapporto', true );
 		$tipologia_rapporto = get_post_meta( $post->ID, 'tipologia_rapporto', true );
 		
-		
+		$media= ($prezzo + $servizio + $qualita + $puntualita + $affidabilita) / 5;
 	
 		$giudizio_review = '';
 		$color = '';
@@ -89,25 +79,24 @@
 			$color = 'red';
 		}
 		
-		$points = get_the_author_meta('media_voto_review',bp_displayed_user_id());
 		
-		if($points != '') 
-		{
-		?>
-			<br/>
-			
-			<div id="new-review-rating" style="border: 1px solid #CCC;display: inline-block;">		
-				<div class="rating-container"><span class="rating-title" style="width:auto;"><?php _e( 'Punteggio medio utente', 'reviews' ); ?></span> 
-					<ul id="prezzo" class='star-rating'>	
-						<li class='current-rating' style="width: <?php echo 25*$points;?>px"></li>
-					</ul>
-				</div>	
-			</div>
-			
-			<br/> 
-		<?php 
-		}
-		?>
+		?>			
+<!-- -->						
+<h2 class="posttitle"><?php the_title(); ?><ul id="prezzo" class='star-rating'>	
+						<li class='current-rating' style="width: <?php echo 25*$media;?>px"></li>
+					</ul></h2>
+
+	<p class="date">
+		<?php printf( __( '%1$s <span>in %2$s</span>', 'buddypress' ), get_the_date(), get_the_category_list( ', ' ) ); ?>
+		<span class="post-utility alignright"><?php edit_post_link( __( 'Edit this entry', 'buddypress' ) ); ?></span>
+	</p>
+
+	<div class="entry">
+		<?php the_content( __( 'Read the rest of this entry &rarr;', 'buddypress' ) ); ?>
+		<?php wp_link_pages( array( 'before' => '<div class="page-link"><p>' . __( 'Pages: ', 'buddypress' ), 'after' => '</p></div>', 'next_or_number' => 'number' ) ); ?>			
+	</div>
+	
+	
 
 	<br/>
 			
@@ -139,7 +128,24 @@
 			<li class='current-rating' style="width: <?php echo 25*$affidabilita;?>px"></li></ul>
 		</div>				
 	</div>	<!-- fine sezione RATING -->		
-
+<?php 
+		
+		if($points != '' && false) 
+		{
+		?>
+			<br/>
+			
+			<div id="new-review-rating" style="border: 1px solid #CCC;display: inline-block;">		
+				<div class="rating-container"><span class="rating-title" style="width:auto;"><?php _e( 'Punteggio medio utente', 'reviews' ); ?></span> 
+					<ul id="prezzo" class='star-rating'>	
+						<li class='current-rating' style="width: <?php echo 25*$points;?>px"></li>
+					</ul>
+				</div>	
+			</div>
+			
+			<br/> 
+		<?php 
+		}?>
 <br/> 
 <br/>
 
