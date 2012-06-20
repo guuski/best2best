17b2a24e5443a9c969ce81e3a69d642dd42a4d9e
giambap <giambap@gmail.com>
2012-06-20 12:35:11 +0000

    better friendship - pannello richiesta amicizia

---
+++ plugins/bp-better-friendship/loader.php
@@ -0,0 +1,62 @@
+<?php
+/*
+Plugin Name: BP Better Friendship
+Plugin URI: 
+Description: Aggiunge la differenziazione del tipo di rapporto commerciale
+Version: 1.0
+Author: Giambattista Pisasale
+Author URI: http://www.insolaria.it/?insoftware
+Licence: GPLv3
+Network: true
+*/
+
+//--------------------------------------------------------------------------------------------------------------------------------------------------		
+//
+//--------------------------------------------------------------------------------------------------------------------------------------------------		
+
+define( 'BPBF_VERSION'	  , '1.0' );
+define( 'BPBF_TEXTDOMAIN' , 'bpbf');
+
+define( 'PLUGINDIR', 'wp-content/plugins' ); // Relative to ABSPATH.  For back compat.
+
+//--------------------------------------------------------------------------------------------------------------------------------------------------		
+//
+//--------------------------------------------------------------------------------------------------------------------------------------------------		
+
+/**
+ * Loads BP Better Friendship files only if BuddyPress is present
+ *
+ * @package BP Better Directories
+ * @since 1.0
+ */
+function bpbf_init() 
+{
+	if ( !function_exists( 'bp_is_active' ) || !bp_is_active( 'xprofile' ) )
+		return;
+	
+	require( dirname( __FILE__ ) . '/bpbfriendship.php' );					//REQUIRE	
+	$bpbf = new BP_better_friendship;												//OBJ
+
+	
+}
+
+//ACTION
+add_action( 'bp_include', 'bpbf_init' );
+
+//--------------------------------------------------------------------------------------------------------------------------------------------------		
+//	LOCALIZZAZIONE / TRADUZIONE 	(Action: init)
+//--------------------------------------------------------------------------------------------------------------------------------------------------		
+
+/**
+ *
+ */
+function bpbf_load_my_textdomain()
+{
+	//load_plugin_textdomain( 'bpbd', dirname( plugin_basename( __FILE__ ) ) . '/languages/' );
+	load_plugin_textdomain(BPBD_TEXTDOMAIN, PLUGINDIR.'/bp-better-friendship/languages','bp-better-friendship/languages');
+	
+	//load_plugin_textdomain(CSP_PO_TEXTDOMAIN, PLUGINDIR.'/codestyling-localization/languages','codestyling-localization/languages');
+}
+//ACTION
+add_action( 'init', 'bpbf_load_my_textdomain');
+?>
