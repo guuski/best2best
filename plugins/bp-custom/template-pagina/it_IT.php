70ed6233b947a19a84605db20dce76e868498e39
iofusin <iofusin@gmail.com>
2012-07-05 16:51:35 +0000

    aggiungere questi file nella cartella di frisco, bisogna solo modificare l'if come sa andrea

---
+++ plugins/bp-custom/template-pagina/it_IT.php
@@ -0,0 +1,31 @@
+<?php
+$lang_IT = array();
+$lang_IT[0]= "Attivit&agrave;";
+$lang_IT[1]= "Messaggi";
+$lang_IT[2]= "Recensioni";
+$lang_IT[3]= "Offerte";
+$lang_IT[4]= "Manca poco, stiamo implementando una nuova funzionalita' che vi permettera' di realizzare una vetrina dei vostri prodotti e servizi. \n\nContinuate a sostenerci. \n - Lo staff.";
+$lang_IT[5]= "Che cosa siamo?";
+$lang_IT[6]= "		
+			<p>
+				<img class='alignleft size-full wp-image-56' title='Best 2 Best Network' src='wp-content/uploads/2012/01/network.jpg' alt='Best 2 Best Network' width='225' height='224'>
+				Best2best &egrave; la novit&agrave; nel mondo del B2B per il settore turismo.
+			</p>
+			
+			<p>
+				Grazie a Best2best infatti, le aziende dell&apos;Alto Adige avranno a disposizione uno strumento valido per incrementare le efficienze delle proprie relazioni d&apos;affari ed approfittare delle occasioni che le aziende vorranno mettere loro a disposizione online.
+			</p>
+						
+			<p>
+				Con Best2Best.it, l&apos;azienda potr&agrave; individuare il partner commerciale che meglio si addice alle caratteristiche ricercate e condividere la propria esperienza con la propria community.
+			</p>
+			
+			<p>&nbsp;</p>
+						
+			<p>
+				Recensioni ed opinioni possono essere indicate con diverso livello di profondit&agrave;, garantendo cos&igrave; a chi ricerca un fornitore la migliore esperienza possibile. 
+				<a href='about'>...continua </a> oppure <a href='registrati'>...registrati! </a>
+			</p>";
+
+?>
+
